f43e14afc88642f3f34326a97a652b257a6d357f
Michael Zhao <mweiyuzhao@gmail.com>
2017-08-22 17:48:22 -0400

    updated .gitignore

--- mys/l/views.py
+++ mys/l/views.py
@@ -15,9 +15,13 @@ def smug_home(request):
     } 
     return HttpResponse(template.render(context, request))
 
-def browse(request):
-    newest_imgs_r1 = ImageDetails.objects.all()[:4]
-    newest_imgs_r2 = ImageDetails.objects.all()[4:8]
+def browse(request, page_num):
+    try:
+        page_num = int(page_num)*2
+    except:
+        return HttpResponse('ERROR: Non-numeric string after /browse')
+    newest_imgs_r1 = ImageDetails.objects.all()[page_num*4:(page_num+1)*4]
+    newest_imgs_r2 = ImageDetails.objects.all()[(page_num+1)*4:(page_num+2)*4]
     template = loader.get_template('l/main.html')
     context = {
        'newest_imgs_r1': newest_imgs_r1,
